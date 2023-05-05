f8e2534c5b316acb51e278c7c43aef86750eaeeb
Nex <nex@nex.sx>
2023-05-06 00:24:20 +0200

    Check that projects path exists before attempting to list it

--- viper/core/projects.py
+++ viper/core/projects.py
@@ -39,6 +39,9 @@ class Projects:
             self.current = Project(PROJECT_DEFAULT)
 
     def list(self) -> list:
+        if not os.path.exists(Storage().projects_path):
+            return []
+
         projects_list = []
         for project_name in os.listdir(Storage().projects_path):
             projects_list.append(os.path.join(Storage().projects_path, project_name))
