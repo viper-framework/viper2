818862a0803e8073318c7a920900a969f6741f3b
Nex <nex@nex.sx>
2023-05-06 00:36:36 +0200

    Starting to add arguments to main entrypoint

--- viper/ui/__init__.py
+++ viper/ui/__init__.py
@@ -1,6 +1,19 @@
+import argparse
+
 from .shell import Shell
 
 
 def main() -> None:
+    parser = argparse.ArgumentParser(
+        prog="viper", description="Binary analysis and management framework"
+    )
+    parser.add_argument(
+        "--modules",
+        "-m",
+        action="store",
+        help="Path to a folder containing Viper modules to load",
+    )
+    args = parser.parse_args()
+
     shell = Shell()
     shell.run()
